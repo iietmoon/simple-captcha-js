984fcc0093aa35a420a251d997b3c71f69c84baa
iietmoon <otmannj56@gmail.com>
2024-03-28 15:07:40 +0000

    chore:adding minor change to add format after each push

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 <p align="center">
   <a href="https://otha.works/simple-captcha">
     <img src="https://i.ibb.co/TWGHw5w/simple-captcha-js.png" alt="Simple captcha" width="125px">
+    <h6>SimpleCaptcha.js</h6>
   </a>
 </p>
 <h2 align="center">Guarding Your Website Forms, One Click at a Time!</h2>
--- dist/core.js
+++ dist/core.js
@@ -1,45 +1,45 @@
 class SimpleCaptcha {
-    constructor() {
-        // Initialize SimpleCaptcha instance
+  constructor() {
+    // Initialize SimpleCaptcha instance
+  }
+
+  ribbon(config) {
+    // Create a new div element for the protection ribbon
+    const simpleCaptchaRibbon = document.createElement("div");
+
+    // Set attributes for the protection ribbon div
+    simpleCaptchaRibbon.id = "simple-captcha-ribbon";
+    simpleCaptchaRibbon.className = "simple-captcha-ribbon";
+
+    // Create an img element for the image
+    const captchaImage = document.createElement("img");
+    captchaImage.src = config.iconSrc; // Provide the path to your image
+
+    // Create an anchor element for the text
+    const ribbonText = document.createElement("a");
+    ribbonText.href = config.link;
+    ribbonText.target = "_blank";
+    ribbonText.textContent = config.title;
+
+    // Append the image and text elements to the protection ribbon div
+    simpleCaptchaRibbon.appendChild(captchaImage);
+    simpleCaptchaRibbon.appendChild(ribbonText);
+
+    // Determine position for the protection ribbon
+    const body = document.getElementsByTagName("body")[0];
+    if (config.position === "bottom-left") {
+      body.insertBefore(simpleCaptchaRibbon, body.firstChild);
+    } else {
+      body.appendChild(simpleCaptchaRibbon);
     }
-
-    ribbon(config) {
-        // Create a new div element for the protection ribbon
-        const simpleCaptchaRibbon = document.createElement("div");
-
-        // Set attributes for the protection ribbon div
-        simpleCaptchaRibbon.id = "simple-captcha-ribbon";
-        simpleCaptchaRibbon.className = "simple-captcha-ribbon";
-
-        // Create an img element for the image
-        const captchaImage = document.createElement("img");
-        captchaImage.src = config.iconSrc; // Provide the path to your image
-
-        // Create an anchor element for the text
-        const ribbonText = document.createElement("a");
-        ribbonText.href = config.link;
-        ribbonText.target = '_blank';
-        ribbonText.textContent = config.title;
-
-        // Append the image and text elements to the protection ribbon div
-        simpleCaptchaRibbon.appendChild(captchaImage);
-        simpleCaptchaRibbon.appendChild(ribbonText);
-
-        // Determine position for the protection ribbon
-        const body = document.getElementsByTagName("body")[0];
-        if (config.position === 'bottom-left') {
-            body.insertBefore(simpleCaptchaRibbon, body.firstChild);
-        } else {
-            body.appendChild(simpleCaptchaRibbon);
-        }
-    }
+  }
 }
 
 const captcha = new SimpleCaptcha();
 
 captcha.ribbon({
-    title: "Website protected!",
-    iconSrc: "/wp-content/plugins/simple-captcha/assets/img/simple-recaptcha.png",
-    link: 'https://github.com/iietmoon/simple-captcha',
-    position: 'bottom-left'
+  title: "Website protected!",
+  iconSrc: "/wp-content/plugins/simple-captcha/assets/img/simple-recaptcha.png",
+  link: "https://github.com/iietmoon/simple-captcha",
+  position: "bottom-left",
 });
