ef55d632851f5a13a26ab9869ffd6e0c32884fd6
iietmoon <otmannj56@gmail.com>
2024-03-28 15:40:56 +0000

    chore: adding style for ribbond

--- dist/core.js
+++ dist/core.js
@@ -9,12 +9,33 @@ class SimpleCaptcha {
 
     simpleCaptchaRibbon.id = "simple-captcha-ribbon";
     simpleCaptchaRibbon.className = "simple-captcha-ribbon";
+    simpleCaptchaRibbon.style.display = "flex";
+    simpleCaptchaRibbon.style.alignItems = "center";
+    simpleCaptchaRibbon.style.justifyContent = "center";
+    simpleCaptchaRibbon.style.width = "fit-content";
+    simpleCaptchaRibbon.style.padding = "10px";
+    simpleCaptchaRibbon.style.backgroundColor = "#f0f0f0";
+    simpleCaptchaRibbon.style.position = "absolute";
+    simpleCaptchaRibbon.style.bottom = "0";
+    simpleCaptchaRibbon.style.right = "15px";
+    simpleCaptchaRibbon.style.gap = "5px";
+    simpleCaptchaRibbon.style.boxShadow =
+      "-2px 1px 14px 0px rgba(151,151,151,0.75)";
+    simpleCaptchaRibbon.style.webkitBoxShadow =
+      "-2px 1px 14px 0px rgba(151,151,151,0.75)";
+    simpleCaptchaRibbon.style.mozBoxShadow =
+      "-2px 1px 14px 0px rgba(151,151,151,0.75)";
 
     // Create an anchor element for the text
     const ribbonText = document.createElement("a");
     ribbonText.href = config.link;
     ribbonText.target = "_blank";
     ribbonText.textContent = config.title;
+    ribbonText.style.color = "black";
+    ribbonText.style.fontFamily = "sans-serif";
+    ribbonText.style.fontWeight = "700";
+    ribbonText.style.textDecoration = "none";
+    ribbonText.style.fontSize = "13px";
 
     // Append the text element to the protection ribbon div
     simpleCaptchaRibbon.appendChild(ribbonText);
@@ -24,6 +45,7 @@ class SimpleCaptcha {
       const captchaImage = document.createElement("img");
       captchaImage.src = "/assets/img/icon.svg";
       captchaImage.alt = "CAPTCHA Icon";
+      captchaImage.style = "width: 15px";
       // Append the image element to the protection ribbon div
       simpleCaptchaRibbon.appendChild(captchaImage);
     }
